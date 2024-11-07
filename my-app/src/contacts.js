6c2a297e1d2ed854eb48ad50a49d0a52defd00dc
ytunkara <yuscheb@yahoo.com>
2024-11-07 01:42:47 +0000

    Homework 5 v1

---
+++ my-app/src/contacts.js
@@ -0,0 +1,35 @@
+const contacts = [
+  {
+    name: "Yusuf Tunkara",
+    imgURL: "https://media.licdn.com/dms/image/v2/C5603AQH0LMNkFt4sjQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1548083297868?e=1736380800&v=beta&t=5t3m_JeP2LeqsBEcE1OLaEwGPL4O_dvgFaCwbjZ1IOs",
+    phone: "+1 (502) 682-3097",
+    email: "tunkar04@email.franklin.edu"
+  },
+  {
+    name: "Jason Zandri",
+    imgURL: "https://media.licdn.com/dms/image/v2/C4E03AQFcvsJoztq5fw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1545000241117?e=1736380800&v=beta&t=Y9C7iZsBSxwg8DfkC7Pvx-okvSgg_DAfNyDf49CK60Y",
+    phone: "+1 (203) 999-9090",
+    email: "Zandri.Jason@microsoft.com"
+  },
+  {
+    name: "Jim Czuprynski",
+    imgURL: "https://media.licdn.com/dms/image/v2/C4E03AQHHRzwywrr3rg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1541186190136?e=1736380800&v=beta&t=-J2iyQPb_SYPARimYeOYTNAKquUr40QypB18sWjHeLA",
+    phone: "+1 (630) 118-2781",
+    email: "Jim.Czuprynski@oraclemasters.com"
+  },
+  {
+    name: "Jeff Smith",
+    imgURL: "https://media.licdn.com/dms/image/v2/D5603AQEdLg7GMDtBGw/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1677598660554?e=1736380800&v=beta&t=j2-HtEqN9X3n2ajpR1TM2GwGZ-_Rr0XXy0Zmt2XK82M",
+    phone: "+1 (817) 220-2201",
+    email: "ThatJeffSmith@oracle.com"
+  },
+  {
+    name: "Srini Ramineni",
+    imgURL: "https://media.licdn.com/dms/image/v2/D5603AQH87iifRSHjsg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1708880929214?e=1736380800&v=beta&t=9NUfotcWjHtp-suLgGqQNQQXdmsLND93E4jbrl58uBQ",
+    phone: "+1 (720) 934-1260",
+    email: "info@DBAuniversity.com "
+  }
+];
+
+export default contacts;
+
